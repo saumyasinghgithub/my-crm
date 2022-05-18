ea468646925d27373d2b9f31dcb980904af883b5
rajesh349 <rajeshs@knowledgesynonyms.net>
2022-05-18 18:42:06 +0530

    Trainer

--- src/components/JoinAsStudent.js
+++ src/components/JoinAsStudent.js
@@ -1,9 +1,38 @@
-import React,{useEffect} from "react";
+import React,{useState, useEffect} from 'react';
+import Utils from './../Utils';
+import axios from 'axios';
+import _ from 'lodash';
 
 const JoinAsStudent = (props) => {
 
     const $ = window.$;
-        
+    const [setError] = useState(false);
+    const submitForm = (e) => {
+
+        setError(false);
+       const frm = e.currentTarget;
+       e.preventDefault();
+   
+       let frmdata = new FormData(frm);
+  
+       if(_.get(frm,'password.value',false)){
+          if(frm.password.value!==frm.conf_password.value){
+            setError("Password verification failed, please verify your password correctly!");
+            return false;
+          }
+       }
+       axios.post(Utils.apiUrl(`admin/add`),frmdata,Utils.apiHeaders())
+       .then(res => {
+          if(res.data.success){
+             window.alert("Record saved successfully!");
+             props.onSubmit();
+             props.onClose();
+          }else{
+             window.alert(res.data.message);
+          }
+       }) 
+   
+     };    
     useEffect(()=>{
         $('.modal').on('show.bs.modal', function (e) {
             $('.modal .modal-dialog').attr('class', 'modal-dialog modal-full  zoomIn  animated');
@@ -35,45 +64,51 @@ const JoinAsStudent = (props) => {
                                 <li>or</li>
                                 <li><a href=""><img className="img-fluid" src="assets/images/mail.png" alt="AD on Email" /></a></li>
                             </ul>
-                            <form>
+                            <form onSubmit={submitForm}>
                                 <div className="row">
                                     <div className="col-sm-6">
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="First name" type="text"/>
+                                            <input className="form-control" name="firstname" placeholder="First name" type="text"/>
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Middle name" type="text"/>
+                                            <input className="form-control" name="middlename" placeholder="Middle name" type="text"/>
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Last name" type="text"/>
+                                            <input className="form-control" name="lastname" placeholder="Last name" type="text"/>
                                         </div>
                                         <div className="form-group downArrow">
-                                            <input className="form-control" placeholder="country" type="text"/>
+                                            <input className="form-control" name="country" placeholder="country" type="text"/>
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Address" type="text"/>
+                                            <input className="form-control" name="address" placeholder="Address" type="text"/>
                                         </div>
                                         <div className="form-group width50">
-                                            <input className="form-control" placeholder="Pin Code" type="text"/>
+                                            <input className="form-control" name="zip" placeholder="Pin Code" type="text"/>
                                         </div>
                                         <div className="form-group width50 width50R downArrow">
-                                            <input className="form-control" placeholder="State" type="text"/>
+                                            <input className="form-control" name="state" placeholder="State" type="text"/>
                                         </div>                                    
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Phone" type="text"/>
+                                            <input className="form-control" name="phone" placeholder="Phone" type="text"/>
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Email" type="email"/>
+                                            <input className="form-control" name="email" placeholder="Email" type="email"/>
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Password" type="password" />
+                                            <input className="form-control" name="password" placeholder="Password" type="password" />
                                         </div>
                                         <div className="form-group">
-                                            <input className="form-control" placeholder="Re-enter Password" type="password" />
+                                            <input className="form-control" name="conf_password" placeholder="Re-enter Password" type="password" />
                                         </div>
-                                        <a  className="btn btnSubmit btnSubmitRed" href="my-profile-edit.php">Sign Up</a>
+                                        <div className="form-group clearfix">
+                                        <div className="custom-control custom-checkbox">
+                                            <input type="checkbox" name="role" className="custom-control-input" value="3" id="customCheck1" checked />
+                                            <label className="custom-control-label" for="customCheck1">Role Active</label>
+                                        </div> 
+                                        </div>
+                                        <button type="submit" className="btn btnSubmit btnSubmitRed">Sign Up</button>
                                         <p>By signing up, you agree to our Terms of Use and Privacy Policy.</p>
                                          <p>Already have an account? <a href="" data-toggle="modal" data-target="#loginModal" data-dismiss="modal">Log In</a></p>
                                     </div>
