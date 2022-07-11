471ff6918b4b859724163b8b31b18fb3005959f5
rajesh349 <rajeshs@knowledgesynonyms.net>
2022-07-11 17:31:30 +0530

    banner and text ediort and base profile images

--- src/components/trainer/AboutForm.js
+++ src/components/trainer/AboutForm.js
@@ -83,6 +83,9 @@ const AboutForm = (props) => {
         height: 200,
         menubar: false,
         }}
+        toolbar= 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl | code'
+        menubar= "tools"
+        plugins='code'
         onEditorChange={onContentChange('biography')}
         />
         </Col>
