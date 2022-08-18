3b67846d16d6b25bb692edb32be42fa3cfb8f68a
rajesh349 <rajeshs@knowledgesynonyms.com>
2022-08-18 16:17:21 +0530

    Sales

--- src/pages/MySales.js
+++ src/pages/MySales.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import DataTable from 'react-data-table-component';
 
 const MySales = (props) => {
@@ -60,6 +60,63 @@ const MySales = (props) => {
         <div className="profile-wrapper">
             <div className="container100">
             <h1>My Sales</h1>
+            <div className="row">
+                <div className="col-lg-3 col-6">
+                    <div className="small-box bg-info">
+                    <div className="inner">
+                    <h3>$ 150</h3>
+                    <p>Total Sales</p>
+                    </div>
+                    <div className="icon">
+                    <i className="ion ion-bag"></i>
+                    </div>
+                    <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-6">
+                    <div className="small-box bg-success">
+                    <div className="inner">
+                    <h3>53<sup className="supTag">%</sup></h3>
+                    <p>My Course Ratio</p>
+                    </div>
+                    <div className="icon">
+                    <i className="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-6">
+                    <div className="small-box bg-warning">
+                    <div className="inner">
+                    <h3>44</h3>
+                    <p>My Students</p>
+                    </div>
+                    <div className="icon">
+                    <i className="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div className="col-lg-3 col-6">
+                    <div className="small-box bg-danger">
+                    <div className="inner">
+                    <h3>65</h3>
+                    <p>Total Order Items</p>
+                    </div>
+                    <div className="icon">
+                    <i className="ion ion-pie-graph"></i>
+                    </div>
+                    <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                
+            </div>
+            <Row>
+                <Col md={12}></Col>
+            </Row>
             <DataTable
                 columns={columns}
                 data={data}
