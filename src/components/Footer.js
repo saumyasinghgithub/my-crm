f0204fb731463dd29a25981ba82cf6adb3ff308a
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2022-09-09 10:46:52 +0530

    module - social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Utils from './../Utils';
+import axios from 'axios';
 
 const Footer = (props) => {
-    return(<>
-    <footer className="footer footerFixed">
-    <div className="container-fluid">
-        <ul className="footerLeft">
-            <li>Copyright © 2022 AUTODIDACT</li>
-            <li><a href={`${process.env.PUBLIC_URL}/term-conditions`}>Terms</a></li>
-            <li><a href={`${process.env.PUBLIC_URL}/privacy-policy`}>Privacy Policy</a></li>
-            <li><a href={`${process.env.PUBLIC_URL}/cookie-policy`}>Cookie Policy</a></li>
-        </ul>
-        <ul className="footerRight">
-            <li><a href=""><img src="assets/images/reddit.png"/></a></li>
-            <li><a href=""><i className="fab fa-facebook-f"></i></a></li>
-            <li><a href=""><i className="fab fa-linkedin-in"></i></a></li>
-            <li><a href=""><i className="fab fa-twitter"></i></a></li>
-        </ul>
-    </div>
-</footer>
-  
+    const [list, setList] = useState({ loading: false, error: false, pageInfo: {}, data: [] });
+
+    const fetchList = () => {
+        setList({ ...list, loading: true })
+        axios.get(Utils.apiUrl('sociallink/list'), Utils.apiHeaders())
+            .then(res => {
+                console.log(res);
+                if (res.data.success) {
+                    setList({ ...list, loading: false, error: false, pageInfo: res.data.pageInfo, data: res.data.data });
+                } else {
+                    setList({ ...list, loading: false, error: res.data.message, pageInfo: {}, data: [] });
+                }
+            })
+    };
+
+    useEffect(fetchList, []);
+
+    return (<>
+        <footer className="footer footerFixed">
+            <div className="container-fluid">
+                <ul className="footerLeft">
+                    <li>Copyright © 2022 AUTODIDACT</li>
+                    <li><a href={`${process.env.PUBLIC_URL}/term-conditions`}>Terms</a></li>
+                    <li><a href={`${process.env.PUBLIC_URL}/privacy-policy`}>Privacy Policy</a></li>
+                    <li><a href={`${process.env.PUBLIC_URL}/cookie-policy`}>Cookie Policy</a></li>
+                </ul>
+                <ul className="footerRight">
+                {list.data.map(record => <li>
+                    <a href={`${record.link}`}><i className={`${record.class}`}></i></a>           
+                </li>)}
+                </ul>
+            </div>
+        </footer>
+
     </>
 
     );
