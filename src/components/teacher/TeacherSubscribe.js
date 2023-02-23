5f6ca55bad472e06af15c507c4a3b3f81e24c44a
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-02-23 14:52:50 +0530

    changes in subscription form

---
+++ src/components/teacher/TeacherSubscribe.js
@@ -0,0 +1,56 @@
+import React, { useRef,useState}  from "react";
+import Utils from '../../Utils';
+import axios from "axios";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup} from 'reactstrap';
+import { useParams } from "react-router-dom";
+
+const TeacherSubscribe = (props) => {
+    const [modal, setModal] = useState(true);
+    const [email, setEmail] = useState('');
+    const toggle = () => setModal(!modal);
+    const frmRef = useRef('SubscribeForm');
+    const { token } = useParams();
+
+    const trainerSlug = Utils.getUserData().slug;
+    const trainerUrl = Utils.getTrainerURL("", trainerSlug);
+    const trainerFullName = Utils.getUserData().firstname + ' ' + Utils.getUserData().middlename + ' ' + Utils.getUserData().lastname;
+
+    const handleSubmit = (event) => {
+        const frm = frmRef.current;
+        event.preventDefault();
+        let frmdata = new FormData(frm);
+        console.log(`Email: ${email}, trainerUrl: ${trainerUrl}`);
+        
+        axios.post(Utils.apiUrl(`trainer/subscribe`),frmdata,Utils.apiHeaders({token: token}))     
+        .then(res => {
+           if(res.data.success){
+           }else{
+              throw(res.data);
+           }           
+        }).catch(err  => {
+            console.log(err);
+        }); 
+    };
+
+    return ( <div>
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Subscription</ModalHeader>
+            <ModalBody>
+                <p>Subscribe to our newsletter and be the first to know about new courses, special offers, and events! from {trainerFullName}</p>
+                <p>To sign up, simply enter your email address below and click "Subscribe".</p>
+                <p>By subscribing, you agree to receive occasional marketing emails from us. We promise not to spam you, and you can unsubscribe at any time.</p>
+                <form ref={frmRef} onSubmit={handleSubmit} method="post">
+                    <FormGroup>
+                        <input type="hidden" name="trainerUrl" value={trainerUrl} />
+                        <input placeholder="Please enter your email here for subscription" type="email" className="input-text form-control" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </FormGroup>
+                </form>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="secondary" type="submit" onClick={handleSubmit}>Subscribe</Button>{' '}
+            </ModalFooter>
+        </Modal>
+    </div>);
+}
+
+export default TeacherSubscribe;
