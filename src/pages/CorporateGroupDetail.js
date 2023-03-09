6f9eec7c7bec0f1379e8b7bda64786b39b26ca2c
surojit19 <surojit19@gmail.com>
2023-03-09 21:37:54 +0530

    mycourses selection and students

--- src/pages/CorporateGroupDetail.js
+++ src/pages/CorporateGroupDetail.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Container, Tab, Row, Col, Button } from "react-bootstrap";
+import { Container, Tab, Row, Col, Form } from "react-bootstrap";
 import UserContext from "./../contexts/UserContext";
 import DataTableGrid from "../components/DataTableGrid";
 import _ from "lodash";
@@ -7,6 +7,8 @@ import { useParams } from "react-router-dom";
 
 const CorporateGroupDetail = () => {
   const [cGroup, setCGroup] = useState({ loading: false, data: false });
+  const [mycourse, setMycourse] = useState("");
+  const [selected, setSelected] = useState([]);
   const { getServerData, setServerData } = useContext(UserContext);
   const { cgid } = useParams();
 
@@ -21,11 +23,14 @@ const CorporateGroupDetail = () => {
     ...style,
   }));
 
+  const handleRowSelected = ({ selectedRows }) => {
+    setSelected(selectedRows);
+  };
+
   const fetchDetail = () => {
     setCGroup({ ...cGroup, loading: true });
     getServerData(`corporate/${cgid}`, true).then((res) => {
       if (res.success) {
-        console.log("picking", _.pick(res, ["cg", "students", "mycourses"]));
         setCGroup({
           ...cGroup,
           loading: false,
@@ -49,13 +54,37 @@ const CorporateGroupDetail = () => {
       <Container fluid className="h-100 p-0">
         <div className="profile-wrapper">
           <div className="container mysale">
+            <h1>Corporate Group: {_.get(cGroup, "data.cg.name", "")}</h1>
+            <Form.Label>
+              <h3>My Courses:</h3>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              onChange={(e) => setMycourse(e.currentTarget.value)}
+              defaultValue={mycourse}
+            >
+              <option value=""> - </option>
+              {_.get(cGroup, "data.mycourses", []).map((course) => (
+                <option key={course.id} value={course.id}>
+                  {course.name} ({course.sku})
+                </option>
+              ))}
+            </Form.Control>
+
             <DataTableGrid
-              title={_.get(cGroup, "data.cg.name", "")}
+              title="Group Students"
               columns={columns}
               data={_.get(cGroup, "data.students", [])}
               selectableRows
+              onSelectedRowsChange={handleRowSelected}
               pagination
             />
+
+            {mycourse !== "" && selected.length > 0 && (
+              <button className="btn btn-lg btn-outline-info">
+                Assign Course to selected students
+              </button>
+            )}
           </div>
         </div>
       </Container>
