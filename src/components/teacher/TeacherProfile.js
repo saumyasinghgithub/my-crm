27afbd168e967810725e971b2606cbef75e52044
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-03-16 12:28:05 +0530

    3. Disable the bread crumb menu then for the time being or hide it - this is done

--- src/components/teacher/TeacherProfile.js
+++ src/components/teacher/TeacherProfile.js
@@ -71,7 +71,7 @@ const TeacherProfile = (props) => {
             <>
               <div className="profile-wrapper">
                 <div className="container">
-                  <nav>
+                  {/*<nav>
                     <ol className="cd-breadcrumb">
                       <li>
                         <a href="/">Home</a>
@@ -82,7 +82,7 @@ const TeacherProfile = (props) => {
                         </em>
                       </li>
                     </ol>
-                  </nav>
+          </nav>*/}
                   {page === "about" && (
                     <>
                       <TeacherSubscribe {...params} />
