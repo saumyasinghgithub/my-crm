4a3f68be5dec1c112076a18d7c4e5726c5d33f21
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-04-12 17:39:40 +0530

    small change

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ const Login = (props) => {
   const hasSubdomain = Utils.hasSubdomain();
   const [mode, setMode] = useState(1); // 1 = login ; 2 = forgot pass 
   //add loader on login page
+  //new line for building code 
   //setting
   const searchParams = new URLSearchParams(window.location.search);
 
