d628349a6097683452ad12e40a48920bc664394d
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-04-27 18:42:28 +0530

    small change for building build

--- src/layouts/LoginLayout.js
+++ src/layouts/LoginLayout.js
@@ -25,8 +25,8 @@ const LoginLayout = ({ children }) => {
             }
           });
         } else {
-          setSitesetting(res.data.data[0]);
-          //setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
+          //setSitesetting(res.data.data[0]);
+          setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
         }
       });
       
