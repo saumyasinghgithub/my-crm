afc7efbe8ad56ffc967ee3a381d840bb9206c7d2
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-04-27 18:29:25 +0530

    worked on site setting form

--- src/layouts/StaticPageLayout.js
+++ src/layouts/StaticPageLayout.js
@@ -15,21 +15,25 @@ const StaticPageLayout = ({ children }) => {
         if (res.data.user_id > 0) {
           console.log(res.data.user_id);
           axios.post(Utils.apiUrl('settings/site-settings'),`&id=${res.data.user_id}`,Utils.apiHeaders()).then((res) => {
-            if (res.data[0]) {
-              console.log(res.data[0]);
-              setSitesetting(res.data[0]);
+            console.log(res.data.data);
+            if (res.data.data[0]) {
+              console.log(res.data.data[0]);
+              setSitesetting(res.data.data[0]);
             } else {
-              setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
+              setSitesetting(res.data.data[0]);
+              //setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
             }
           });
         } else {
-          setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
+          setSitesetting(res.data.data[0]);
+          //setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
         }
       });
       
     } 
-    if((Utils.subdomain() === 'localhost:3002')){
-      setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
+    if((Utils.subdomain() === 'localhost:3002') || (Utils.subdomain() === 'kstverse.com')){
+      setSitesetting(res.data.data[0]);
+      //setSitesetting({"firstname":"KS TVERSE ","middlename":"","lastname":"Davis","id":2,"trainer_id":55,"company_name":"KSTVERSE","site_title":"RescueRN","logo":null,"contact_email":null,"contact_address":null,"contact_phone":null,"copywrite_text":"by KS TVERSE","created_at":"2023-04-26T05:41:26.000Z","updated_at":null});
     }
   }
   useEffect(callbackfn,[]);
---
+++ src/pages/SiteSettings.js
@@ -0,0 +1,209 @@
+import { useEffect, useContext, useState } from "react";
+import {
+  Form,
+  Alert,
+  Spinner,
+  Container,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
+import _ from "lodash";
+import { Editor } from "@tinymce/tinymce-react";
+import UserContext from "../contexts/UserContext";
+import Utils from "../Utils";
+import axios from "axios";
+const SiteSettings = () => {
+  const [mysitesettings, setMysitesettings] = useState({});
+  const [saving, setSaving] = useState(false);
+  const [response, setResponse] = useState({ success: false, message: "" });
+  const { getServerData, setServerData } = useContext(UserContext);
+
+  const onSave = (e) => {
+    const frm = e.currentTarget;
+    e.preventDefault();
+    let frmdata = new FormData(frm);
+    setSaving(true);
+    frmdata.append(
+      "contact_address",
+      _.get(mysitesettings, "contact_address", "")
+    );
+    setServerData("settings/add-site-settings", frmdata).then((res) => {
+      setSaving(false);
+      setResponse(res);
+    });
+
+    getServerData(`settings/site-settings`, true)
+      .then((tData) => {
+        setMysitesettings(tData);
+      })
+      .catch((msg) => {
+        setMysitesettings({ success: false, message: msg });
+      });
+  };
+
+  useEffect(() => {
+    axios.get(Utils.apiUrl('settings/site-settings'),Utils.apiHeaders()).then((res) => {
+      console.log(res.data.type);
+      if(res.data.type === 'default'){
+        setMysitesettings({});
+      } else {
+        setMysitesettings(res.data.data[0]);
+      }
+      
+    });
+  }, []);
+
+  const onContentChange = (fld) => (value) => {
+    let c = { ...mysitesettings };
+    c[fld] = value;
+    setMysitesettings(c);
+  };
+  const photoUploader = (fld, title) => {
+    return (
+      <>
+        <Form.Label>{title}</Form.Label>
+        <Form.Control
+          type="file"
+          size="lg"
+          name={fld}
+          accept=".jpeg,.png,.PNG,.jpg;"
+        />
+        <div className="text-center">
+          {!_.isEmpty(_.get(mysitesettings, fld, "")) && (
+            <img
+              src={`${process.env.REACT_APP_API_URL}/uploads/${fld}/${mysitesettings[fld]}`}
+              className="thumbnail mt-3"
+            />
+          )}
+        </div>
+      </>
+    );
+  };
+  return (
+    <Container fluid className="h-100 p-0">
+      <div className="profile-wrapper">
+        <div className="container">
+          <h1>Site Settings</h1>
+          <p className="alert alert-warning disclaimer mt-3 mb-3">
+            All fields are optional and can override default settings. Please
+            use your judgment when filling them out. Leaving a field blank uses
+            the default setting. Your completion of any field acknowledges
+            acceptance of overriding the default settings. Thank you.
+          </p>
+          <Col md={12}>
+            <Form onSubmit={onSave}>
+              <Form.Control
+                type="hidden"
+                name="id"
+                defaultValue={_.get(mysitesettings, "id", "")}
+              />
+              <Row>
+                <Col md={6} className="mt-3">
+                  <Form.Label>Company Name: </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="company_name"
+                    placeholder="Enter name of your company"
+                    defaultValue={_.get(mysitesettings, "company_name", "")}
+                  />
+                </Col>
+                <Col md={6} className="mt-3">
+                  <Form.Label>Page Title: </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="site_title"
+                    placeholder="Enter your site title"
+                    defaultValue={_.get(mysitesettings, "site_title", "")}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6} className="mt-3">
+                  <Form.Label>Contact Phone: </Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="contact_phone"
+                    placeholder="Enter your contact phone"
+                    defaultValue={_.get(mysitesettings, "contact_phone", "")}
+                  />
+                </Col>
+                <Col md={6} className="mt-3">
+                  <Form.Label>Contact Email: </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="contact_email"
+                    placeholder="Enter your contact email"
+                    defaultValue={_.get(mysitesettings, "contact_email", "")}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6} className="mt-3">
+                  {photoUploader(
+                    "logo",
+                    "Upload logo (Image dimension should be 88cm x 40cm)"
+                  )}
+                </Col>
+                <Col md={6} className="mt-3">
+                  <Form.Label>Contact Address: </Form.Label>
+                  <Editor
+                    apiKey={process.env.TINYMCE_API_KEY}
+                    value={
+                      _.isEmpty(_.get(mysitesettings, "contact_address", ""))
+                        ? ""
+                        : mysitesettings.contact_address
+                    }
+                    init={{
+                      height: 200,
+                      menubar: false,
+                    }}
+                    toolbar="undo redo | bold italic underline strikethrough | code | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl"
+                    menubar="tools"
+                    plugins="code"
+                    onEditorChange={onContentChange("contact_address")}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="mt-3">
+                  <Form.Label>Copywrite Text: </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="copywrite_text"
+                    placeholder="Enter your copywrite text"
+                    defaultValue={_.get(mysitesettings, "copywrite_text", "")}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="mt-3 text-right">
+                  {saving && (
+                    <>
+                      Saving.. <Spinner animation="border" />
+                    </>
+                  )}
+                  {!saving && response.message === "" && (
+                    <Button type="submit" className="profile-save">
+                      Save
+                    </Button>
+                  )}
+                  {!saving && response.message !== "" && (
+                    <Alert
+                      variant={response.success ? "info" : "danger"}
+                      className="p-3 mt-2 text-center"
+                    >
+                      {response.message}
+                    </Alert>
+                  )}
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default SiteSettings;
